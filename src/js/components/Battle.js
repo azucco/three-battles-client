8bf4da853ae2b101e1ae3e986ff4f21a716a2417
azucco <alessandro.zucco@gmail.com>
2020-02-19 23:11:51 +0100

    battle component optimization

--- src/js/components/Battle.js
+++ src/js/components/Battle.js
@@ -4,75 +4,47 @@ import "../../styles/battle.css";
 
 function Battle() {
 
-  const [redInfantry, setRedInfantry] = useState(0)
-  const [blueInfantry, setBlueInfantry] = useState(0)
-  const [greenInfantry, setGreenInfantry] = useState(0)
-  const [yellowInfantry, setYellowInfantry] = useState(0)
-
-  const [redArchery, setRedArchery] = useState(0)
-  const [blueArchery, setBlueArchery] = useState(0)
-  const [greenArchery, setGreenArchery] = useState(0)
-  const [yellowArchery, setYellowArchery] = useState(0)
-
-  const [redCavalry, setRedCavalry] = useState(0)
-  const [blueCavalry, setBlueCavalry] = useState(0)
-  const [greenCavalry, setGreenCavalry] = useState(0)
-  const [yellowCavalry, setYellowCavalry] = useState(0)
+  const [redArmy, setRedArmy] = useState([0, 0, 0])
+  const [blueArmy, setBlueArmy] = useState([0, 0, 0])
+  const [greenArmy, setGreenArmy] = useState([0, 0, 0])
+  const [yellowArmy, setYellowArmy] = useState([0, 0, 0])
 
   const lines = [
     {
       color: "red",
-      infantry: redInfantry,
-      cavalry: redCavalry,
-      archery: redArchery,
-      setInfantry: setRedInfantry,
-      setCavalry: setRedCavalry,
-      setArchery: setRedArchery
+      army: redArmy,
+      setArmy: setRedArmy
     },
     {
       color: "blue",
-      infantry: blueInfantry,
-      cavalry: blueCavalry,
-      archery: blueArchery,
-      setInfantry: setBlueInfantry,
-      setCavalry: setBlueCavalry,
-      setArchery: setBlueArchery
+      army: blueArmy,
+      setArmy: setBlueArmy
     },
     {
       color: "green",
-      infantry: greenInfantry,
-      cavalry: greenCavalry,
-      archery: greenArchery,
-      setInfantry: setGreenInfantry,
-      setCavalry: setGreenCavalry,
-      setArchery: setGreenArchery
+      army: greenArmy,
+      setArmy: setGreenArmy
     },
     {
       color: "yellow",
-      infantry: yellowInfantry,
-      cavalry: yellowCavalry,
-      archery: yellowArchery,
-      setInfantry: setYellowInfantry,
-      setCavalry: setYellowCavalry,
-      setArchery: setYellowArchery
-    },
+      army: yellowArmy,
+      setArmy: setYellowArmy
+    }
   ]
 
   lines.sort(function (a, b) {
-    return a.archery - b.archery;
+    return parseInt(a.archery) - parseInt(b.archery);
   });
 
   return (
     <div className="col-sm-4 battle">
       {lines.map(line => {
+        const total = line.army.reduce((a, b) => a + b)
         return <Line
           color={line.color}
-          infantry={line.infantry}
-          cavalry={line.cavalry}
-          archery={line.archery}
-          setInfantry={line.setInfantry}
-          setCavalry={line.setCavalry}
-          setArchery={line.setArchery}
+          army={line.army}
+          setArmy={line.setArmy}
+          total={total}
         />
       })}
     </div>
--- src/js/components/Line.js
+++ src/js/components/Line.js
@@ -4,13 +4,25 @@ import "../../styles/line.css";
 function Line(props) {
 
   const rowClass = "row " + props.color;
-  const total = parseInt(props.infantry) + parseInt(props.cavalry) + parseInt(props.archery);
-
+  let army = props.army;
+  let setArmy = props.setArmy;
+  const total = props.total;
+  
   return (
     <div className={rowClass}>
-      <input type="number" className="form-control col-sm-3 trasparent" value={props.infantry} onChange={e => props.setInfantry(e.target.value)} />
-      <input type="number" className="form-control col-sm-3 trasparent" value={props.cavalry} onChange={e => props.setCavalry(e.target.value)} />
-      <input type="number" className="form-control col-sm-3 trasparent" value={props.archery} onChange={e => props.setArchery(e.target.value)} />
+      {army.map((unit, index) => {
+        return (
+          <input
+            type="number"
+            className="form-control col-sm-3 trasparent"
+            value={unit}
+            onChange={e => {
+              army[index] = parseInt(army[index]) + parseInt(e.target.value);
+              return setArmy(army)
+            }}
+          />
+        )
+      })}
       <input type="number" className="form-control col-sm-3" value={total} disabled />
     </div>
   );
